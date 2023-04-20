93fe517d7dffb01098072ce56278a92f06e9dd79
SachiniSiriwardene <sachinisiriwardene@gmail.com>
2023-04-20 11:56:50 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 redis-server --daemonize yes
 
 
-java -Xdiag -cp "sachini-redis_choreo_docker-0.1.0.jar:jars/*" 'sachini/redis_choreo_docker/0/$_init'
+java -Xdiag -cp "bfsitest-greeting_service-3.1.0:jars/*" 'bfsitest/greeting_service/0/$_init'
 
 
