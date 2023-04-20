956ac34b4642844ca362dcff05e978d5f8ee20ec
SachiniSiriwardene <sachinisiriwardene@gmail.com>
2023-04-20 12:01:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,5 @@
 redis-server --daemonize yes
 
 
-java -Xdiag -cp "bfsitest-greeting_service-3.1.0:jars/*" 'bfsitest/greeting_service/0/$_init'
-
+java -Xdiag -cp "bfsitest-greeting_service-3.1.0.jar:jars/*" 'bfsitest/greeting_service/3/$_init
 
